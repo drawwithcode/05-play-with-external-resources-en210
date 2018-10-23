f4548b422a9ebafa97599551bdf270244d9f38c7
Ennio Sorrentino <enniosorretntino@gmail.com>
2018-10-23 23:21:39 +0200

    as5

--- sketch.js
+++ sketch.js
@@ -1,11 +1,75 @@
+var fft;
+var newmsc;
+var filter;
+var fltrbss;
+var img;
 function preload(){
-  // put preload code here
+  newmsc=loadSound('./assets/BACH.mp3')
+  img=loadImage('./assets/sfondo.jpg')
 }
 
 function setup() {
-  // put setup code here
+   createCanvas(windowWidth,windowHeight);
+   noFill();
+
+
+frameRate(10)
+
+   fft = new p5.FFT(0,128);
+   vl= new p5.Amplitude();
+   newmsc.play()
+   fft.setInput(newmsc);
 }
 
 function draw() {
-  // put drawing code here
+
+
+  var filter = new p5.LowPass();
+
+
+  if(mouseX<width/10){fltrbss=1}
+  else if(mouseX<width/3){fltrbss=500}
+  else{fltrbss=30000}
+
+  filter.set(fltrbss);
+  newmsc.disconnect();
+  newmsc.connect(filter);
+
+
+
+
+   var spectrum = fft.analyze();
+   var vol= vl.getLevel()
+   var volmap = map(vol,-1,1,0,255)
+
+
+           image(img,0-map(vol,0,1,0,2000),0-map(vol,0,1,0,2000),width+1000,height+1000);
+
+var test=300
+
+
+strokeWeight(0)
+
+
+   for (h = 0; h<spectrum.length; h++) {
+     for (i = 0; i<spectrum.length; i++) {
+     if(spectrum[h]<10){stroke('red')}
+     else if(spectrum[i]==0){stroke('yellow')}
+     else{fill(spectrum[i],spectrum[h],150)
+
+     if(fltrbss==1){ellipse((spectrum[i]/10)+spectrum[h],h*25,20)}
+     if(fltrbss==500){ellipse(spectrum[i]+spectrum[h],h*25,20)}
+     if(fltrbss==30000){ellipse((spectrum[i]*4)+spectrum[h],h*25,20)}
+    }
+       ;}}
+
+// 
+//     console.log()
+//
+// text(mouseX*20,600,100)
+// text(spectrum[test],600,120)
+// text(volmap,600,140)
+// text(map(mouseX,0,width,10,22050),600,160)
+colorMode(RGB)
+noFill()
 }
